375245b7a9a4fe9ed102ed28a6e9dfc8af912392
ManzarSiddique <manzar0613@gmail.com>
2018-03-19 12:42:33 +0530

    Initial

--- app/src/main/java/com/example/kotlinhelloworld/test/Test.java
+++ app/src/main/java/com/example/kotlinhelloworld/test/Test.java
@@ -5,4 +5,19 @@ package com.example.kotlinhelloworld.test;
  */
 
 public class Test {
+
+    static String firstName = "makhmoorshakir";
+    static String secondName = "substring(makhmoorshakir, 1, 5)";
+    String[] temp;
+
+    public static void main(String[] args){
+        int asciiOne = 01;
+//        System.out.println("Ascii to string : "+Character.toString((char)01));
+
+//        System.out.println("Sub string is : "+javaSubString(firstName, 10, 3));
+//        test();
+//        String[] temp = {"data111111111,1,5", "data22222222222,2,6", "data3333333333,3,7"};
+//        example(temp);
+
+    }
 }
