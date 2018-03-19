c74e690534354f0817967f78f1b77748e2ec98d2
ManzarSiddique <manzar0613@gmail.com>
2018-03-19 12:44:13 +0530

    Initial

--- app/src/main/java/com/example/kotlinhelloworld/test/Test.java
+++ app/src/main/java/com/example/kotlinhelloworld/test/Test.java
@@ -6,3 +6,13 @@ package com.example.kotlinhelloworld.test;
 
 public class Test {
 }
+    static String firstName = "makhmoorshakir";
+    static String secondName = "substring(makhmoorshakir, 1, 5)";
+    String[] temp;
+
+    public static void main(String[] args){
+        int asciiOne = 01;
+//        System.out.println("Ascii to string : "+Character.toString((char)01));
+
+    }
+}
